73905030660e076bdcb44f2504ca1d46bd3243d7
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-10-06 23:34:05 +0400

    Moved rules manager to separate class (block parser only)

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -6,7 +6,7 @@ var Remarkable = require('../');
 
 var md = new Remarkable();
 
-var data = fs.readFileSync(path.join(__dirname, '/samples/lorem1.txt', 'utf8'));
+var data = fs.readFileSync(path.join(__dirname, '/samples/lorem1.txt'), 'utf8');
 
 for (var i = 0; i < 20000; i++) {
   md.render(data);
