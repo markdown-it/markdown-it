337b08ee836a8f2d2032d02e909680bd547dd5a8
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-10-07 20:42:08 +0400

    Fix benchmark options (enable html)

--- benchmark/implementations/current/index.js
+++ benchmark/implementations/current/index.js
@@ -1,7 +1,12 @@
 'use strict'
 
 var Remarkable = require('../../../');
-var md = new Remarkable();
+var md = new Remarkable({
+  html: true,
+  xhtml: true,
+  breaks: false,
+  langprefix: 'language-'
+});
 
 exports.run = function(data) {
   return md.render(data);
--- benchmark/profile.js
+++ benchmark/profile.js
@@ -1,12 +1,19 @@
+#!/usr/bin/env node
+/*eslint no-console:0*/
 'use strict';
 
 var fs = require('fs');
 var path = require('path');
 var Remarkable = require('../');
 
-var md = new Remarkable();
+var md = new Remarkable({
+  html: true,
+  xhtml: true,
+  breaks: false,
+  langprefix: 'language-'
+});
 
-var data = fs.readFileSync(path.join(__dirname, '/samples/lorem1.txt'), 'utf8');
+var data = fs.readFileSync(path.join(__dirname, '/samples/cdata.md'), 'utf8');
 
 for (var i = 0; i < 20000; i++) {
   md.render(data);
