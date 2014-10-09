1dcdd61850d22a62ab70fb0e9ab148250016b077
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-10-09 10:29:29 +0400

    Typographer: enchanced plain rules

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -13,7 +13,7 @@ var md = new Remarkable({
   langPrefix: 'language-'
 });
 
-var data = fs.readFileSync(path.join(__dirname, '/samples/cdata.md'), 'utf8');
+var data = fs.readFileSync(path.join(__dirname, '/samples/lorem1.txt'), 'utf8');
 
 for (var i = 0; i < 20000; i++) {
   md.render(data);
