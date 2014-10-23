d02fc8a32b49c57f72fa687f140b2e6829a8d6ca
Vitaly Puzrin <vitaly@rcdesign.ru>
2014-10-23 16:56:05 +0400

    base64 encode demo permalinks to avoid broken content for github postings

--- demo/assets/index.js
+++ demo/assets/index.js
@@ -60,9 +60,9 @@
     try {
       if (source) {
         // serialize state - source and options
-        permalink.href = '#md=' + encodeURIComponent(JSON.stringify({
-          defaults: _.omit(defaults, 'highlight'),
-          source: source
+        permalink.href = '#md64=' + window.btoa(JSON.stringify({
+          source: source,
+          defaults: _.omit(defaults, 'highlight')
         }));
       } else {
         permalink.href = '';
@@ -80,9 +80,17 @@
     });
 
     // Restore content if opened by permalink
-    if (location.hash && location.hash.toString().slice(0,4) === '#md=') {
+    if (location.hash && /^(#md=|#md64=)/.test(location.hash)) {
       try {
-        var cfg = JSON.parse(decodeURIComponent(location.hash.slice(4)));
+        var cfg;
+
+        if (/^#md64=/.test(location.hash)) {
+          cfg = JSON.parse(window.atob(location.hash.slice(6)));
+        } else {
+          // Legacy mode for old links. Those become broken in github posts,
+          // so we switched to base64 encoding.
+          cfg = JSON.parse(decodeURIComponent(location.hash.slice(4)));
+        }
 
         if (_.isString(cfg.source)) {
           $('.source').val(cfg.source);
