88c6e0f8e6fd567c70ffabbc1e9ce7b980d2e3a9
Vitaly Puzrin <vitaly@rcdesign.ru>
2016-01-06 18:24:14 +0300

    Demo: updated highlighting style

--- support/demo_template/index.js
+++ support/demo_template/index.js
@@ -90,6 +90,31 @@ function mdInit() {
     return window.twemoji.parse(token[idx].content);
   };
 
+
+  mdHtml.renderer.rules.fence = function (tokens, idx, options, env, slf) {
+    var escapeHtml = mdHtml.utils.escapeHtml,
+        unescapeAll = mdHtml.utils.unescapeAll,
+        token = tokens[idx],
+        info = token.info ? unescapeAll(token.info).trim() : '',
+        langName = '',
+        highlighted;
+
+    if (info) {
+      langName = info.split(/\s+/g)[0];
+      token.attrPush([ 'class', options.langPrefix + langName ]);
+    }
+
+    if (options.highlight) {
+      highlighted = options.highlight(token.content, langName) || escapeHtml(token.content);
+    } else {
+      highlighted = escapeHtml(token.content);
+    }
+
+    return  '<pre class="hljs"><code' + slf.renderAttrs(token) + '>'
+          + highlighted
+          + '</code></pre>\n';
+  };
+
   //
   // Inject line numbers for sync scroll. Notes:
   //
