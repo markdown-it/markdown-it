e113df738e4d164b1a5bdd0e345a7847c84d29bf
Samy Pessé <samypesse@gmail.com>
2016-02-24 16:21:12 +0100

    Add position and size for tables

--- test/annotation.js
+++ test/annotation.js
@@ -2,7 +2,7 @@
 
 var assert = require('chai').assert;
 
-function asertTokenContent(src, token, content) {
+function assertTokenContent(src, token, content) {
   assert.strictEqual(src.slice(token.position, token.position + token.size), content);
 }
 
@@ -63,15 +63,15 @@ describe.only('Annotation', function() {
 
     // First heading
     assert.strictEqual(tokens[0].position, 0);
-    asertTokenContent(src, tokens[0], '');
-    asertTokenContent(src, tokens[1], 'Hello');
-    asertTokenContent(src, tokens[2], '=====\n');
+    assertTokenContent(src, tokens[0], '');
+    assertTokenContent(src, tokens[1], 'Hello');
+    assertTokenContent(src, tokens[2], '=====\n');
 
     // Second heading
     assert.strictEqual(tokens[3].position, 13);
     assert.strictEqual(tokens[3].size, 0);
-    asertTokenContent(src, tokens[4], 'World');
-    asertTokenContent(src, tokens[5], '=======');
+    assertTokenContent(src, tokens[4], 'World');
+    assertTokenContent(src, tokens[5], '=======');
   });
 
   it('should annotate code blocks', function () {
@@ -83,6 +83,62 @@ describe.only('Annotation', function() {
 
     assert.strictEqual(tokens[4].position, 18);
     assert.strictEqual(tokens[4].size, 9);
+  });
+
+  it('should annotate tables', function () {
+    var src = 'Test:\n\n' +
+              ' | Type | Message |\n' +
+              ' | ---- | ------- |\n' +
+              '| Hello | World\n' +
+              ' | Bonjour | Monde |\n';
+    var tokens = md.parse(src);
+    assert.strictEqual(tokens.length, 33);
+
+    // Begin
+    assert.strictEqual(tokens[3].position, 7);
+    assert.strictEqual(tokens[3].size, 0);
+
+    // head (open)
+    assert.strictEqual(tokens[4].position, 7);
+    assert.strictEqual(tokens[4].size, 0);
+
+    // head -> TR (open)
+    assert.strictEqual(tokens[5].position, 7);
+    assert.strictEqual(tokens[5].size, 0);
+
+    // head -> columns
+    assertTokenContent(src, tokens[6], '|');
+    assertTokenContent(src, tokens[7], ' Type ');
+    assertTokenContent(src, tokens[8], '');
+    assertTokenContent(src, tokens[9], '|');
+    assertTokenContent(src, tokens[10], ' Message ');
+    assertTokenContent(src, tokens[11], '|');
+
+    // head -> TR (close)
+    assert.strictEqual(tokens[12].position, 26);
+    assert.strictEqual(tokens[12].size, 0);
+
+    // head (close)
+    assertTokenContent(src, tokens[13], ' | ---- | ------- |');
+
+    // body (open)
+    assert.strictEqual(tokens[14].position, 47);
+    assert.strictEqual(tokens[14].size, 0);
+
+    // body -> rows
+    assertTokenContent(src, tokens[16], '|');
+    assertTokenContent(src, tokens[17], ' Hello ');
+    assertTokenContent(src, tokens[18], '');
+    assertTokenContent(src, tokens[19], '|');
+    assertTokenContent(src, tokens[20], ' World');
+    assertTokenContent(src, tokens[21], '\n');
+
+    assertTokenContent(src, tokens[24], '|');
+    assertTokenContent(src, tokens[25], ' Bonjour ');
+    assertTokenContent(src, tokens[26], '');
+    assertTokenContent(src, tokens[27], '|');
+    assertTokenContent(src, tokens[28], ' Monde ');
+    assertTokenContent(src, tokens[29], '|');
   });
 
 
