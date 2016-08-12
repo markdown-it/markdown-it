markdown-it/markdown-it

commit b7c868b64b710c8e706191d89a6dd80fcbbc9166

Renderer: token stream should stay immutable, close #260

lib/renderer.js
@@ -32,11 +32,10 @@ default_rules.fence = function (tokens, idx, options, env, slf) {
   var token = tokens[idx],
       info = token.info ? unescapeAll(token.info).trim() : '',
       langName = '',
-      highlighted;
+      highlighted, i, tmpAttrs, tmpToken;
 
   if (info) {
     langName = info.split(/\s+/g)[0];
-    token.attrJoin('class', options.langPrefix + langName);
   }
 
   if (options.highlight) {
@@ -48,6 +47,30 @@ default_rules.fence = function (tokens, idx, options, env, slf) {
   if (highlighted.indexOf('<pre') === 0) {
     return highlighted + '\n';
   }
+
+  // If language exists, inject class gently, without mudofying original token.
+  // May be, one day we will add .clone() for token and simplify this part, but
+  // now we prefer to keep things local.
+  if (info) {
+    i        = token.attrIndex('class');
+    tmpAttrs = token.attrs ? token.attrs.slice() : [];
+
+    if (i < 0) {
+      tmpAttrs.push([ 'class', options.langPrefix + langName ]);
+    } else {
+      tmpAttrs[i] += ' ' + options.langPrefix + langName;
+    }
+
+    // Fake token just to render attributes
+    tmpToken = {
+      attrs: tmpAttrs
+    };
+
+    return  '<pre><code' + slf.renderAttrs(tmpToken) + '>'
+          + highlighted
+          + '</code></pre>\n';
+  }
+
 
   return  '<pre><code' + slf.renderAttrs(token) + '>'
         + highlighted

commit e9306a572cd4938a9140f4067a95327d7f25b53b

fence renderer: fix concat of class array
Before:

  <pre c="l"><code>...

Now:

  <pre class="someClass language-python><code>...

Bug introduced in b7c868b64b710c8e706191d89a6dd80fcbbc9166

lib/renderer.js
@@ -68,7 +68,7 @@ default_rules.fence = function (tokens, idx, options, env, slf) {
     if (i < 0) {
       tmpAttrs.push([ 'class', options.langPrefix + langName ]);
     } else {
-      tmpAttrs[i] += ' ' + options.langPrefix + langName;
+      tmpAttrs[i][1] += ' ' + options.langPrefix + langName;
     }
 
     // Fake token just to render attributes