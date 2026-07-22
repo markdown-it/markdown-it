e4a1ee079f55b4fdfb2480cf6e6635f91278d6f9
Vitaly Puzrin <vitaly.puzrin@gmail.com>
2026-07-23 00:16:54 +0300

    Bump deps (to use built-in types)

--- src/external.d.ts
+++ src/external.d.ts
@@ -1,12 +1,3 @@
-declare module 'uc.micro' {
-  export const Any: RegExp
-  export const Cc: RegExp
-  export const Cf: RegExp
-  export const P: RegExp
-  export const S: RegExp
-  export const Z: RegExp
-}
-
 declare module 'punycode.js' {
   interface Punycode {
     toASCII(input: string): string
