1835c10b4edc4bb72bda5aef36a95dc1965222f5
Campbell <campbell.wass@gmail.com>
2026-07-24 14:52:22 +1000

    Optimize newline regex in normalize rule

--- src/rules_core/normalize.ts
+++ src/rules_core/normalize.ts
@@ -3,14 +3,14 @@
 import type StateCore from './state_core.ts'
 
 // https://spec.commonmark.org/0.29/#line-ending
-const NEWLINES_RE = /\r\n?|\n/g
+const UNNORMALIZED_NEWLINE_RE = /\r\n?/g
 const NULL_RE = /\0/g
 
 export default function normalize (state: StateCore): void {
   let str
 
   // Normalize newlines
-  str = state.src.replace(NEWLINES_RE, '\n')
+  str = state.src.replace(UNNORMALIZED_NEWLINE_RE, '\n')
 
   // Replace NULL characters
   str = str.replace(NULL_RE, '\uFFFD')
